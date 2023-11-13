73da90d8d94a45aba04aac9eda51e8a3e0fa7b61
Gabor Nagy <11444120+hellogabor@users.noreply.github.com>
2023-11-13 21:43:44 +0100

    add basic calculator functions

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-const add = (a, b) => {
-    
-    
-    (add(a, b) => (a + b))
+let a
+let b
+let operator
+
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const multiply = function(a, b) {
+	return a * b;
+};
+
+const divide = function(a, b) {
+	if (b == 0) {
+        return 'ERROR';
+    } else {
+        return a / b;
+    }
+};
+
+const operate = function(a, operator, b) {
+    if (operator == '+') {
+        return add(a, b);
+    } else if (operator == '-') {
+        return subtract(a, b);
+    } else if (operator == '*') {
+        return multiply(a, b);
+    } else if (operator == '/') {
+        return divide(a, b);
+}
